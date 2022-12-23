78da884073c8f13f6e2bc1ea5ab28e6b854edd7b
cadacada <120771400+cadacada@users.noreply.github.com>
2022-12-22 23:46:50 -0700

    my first code

--- Backend.tf
+++ Backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "myfirstbackendbucket"
     key    = "demo1.tfstate"
-    region = "us-west-2"
+    region = "us-east-2"
     profile = "default"
     dynamodb_table = "myfirstDBLock"
   }
